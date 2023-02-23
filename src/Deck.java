f72441f3a1ab74f0d8aa95d813308454e18caee3
David Macy <dmacy@carroll.edu>
2023-02-22 19:12:37 -0700

    added a dealTopCard method which will allow the top card to a hand object

--- src/Deck.java
+++ src/Deck.java
@@ -3,28 +3,32 @@ import java.util.Collections;
 import java.util.List;
 
 public class Deck {
-    private final List<Card> cards;
+    private final List<Card> deck;
 
     public Deck() {
-        this.cards = new ArrayList<>();
+        this.deck = new ArrayList<>();
         for (Card.Suit suit : Card.Suit.values()) {
             for (Card.FaceName faceName : Card.FaceName.values()) {
                 String imagePath = "src/images/" + faceName.name().toLowerCase() + "_of_" + suit.name().toLowerCase() + ".png";
                 Card card = new Card(suit, faceName, imagePath);
-                this.cards.add(card);
+                this.deck.add(card);
             }
         }
-        Collections.shuffle(this.cards);
+        Collections.shuffle(this.deck);
     }
 
-    public List<Card> getCards() {
-        return this.cards;
+    public List<Card> getDeck() {
+        return this.deck;
     }
 
     public Card getTopCard() {
-        Card topCard = cards.get(0);
-        cards.remove(0);
+        Card topCard = deck.remove(0);
         return topCard;
+    }
+
+    public void dealTopCard(Hand hand) {
+        Card topCard = deck.remove(0);
+        hand.getHand().add(topCard);
     }
 
 }
