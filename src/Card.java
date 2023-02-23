e25e3b94de36610ca1b0a363fa298205c9acedd0
Abby Brown <abrown01@carroll.edu>
2023-02-23 12:59:26 -0700

    Added another comment

--- src/Card.java
+++ src/Card.java
@@ -1,28 +1,19 @@
 import javax.swing.ImageIcon;
+import java.util.Hashtable;
 
 public class Card {
     private final Suit suit;
     private final FaceName faceName;
     private final ImageIcon image;
+//    private final Hashtable<Card, Integer> deck = makeDeck();
 
     public enum Suit {
-        Coins,
-        Sticks,
-        Cups,
-        Swords
+        Coins, Sticks, Cups, Swords
     }
 
+    // Added a comment
     public enum FaceName {
-        Ace,
-        Two,
-        Three,
-        Four,
-        Five,
-        Six,
-        Seven,
-        Jack,
-        Horse,
-        King
+        Ace, Two, Three, Four, Five, Six, Seven, Jack, Horse, King
     }
 
     public Card(Suit suit, FaceName faceName, String imagePath) {
@@ -30,6 +21,14 @@ public class Card {
         this.faceName = faceName;
         this.image = new ImageIcon(imagePath);
     }
+
+//    public Hashtable makeDeck() {
+//        Hashtable<Card, Integer> deck = new Hashtable<>();
+//
+//
+//
+//        return deck;
+//    }
 
     public Suit getSuit() {
         return suit;
