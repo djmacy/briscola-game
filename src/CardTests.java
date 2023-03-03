7310a6cf3a7b32153ccc123dd67e167000e73d45
Abby Brown <abrown01@carroll.edu>
2023-03-03 10:55:40 -0700

    Start making card tests

---
+++ src/CardTests.java
@@ -0,0 +1,12 @@
+public class CardTests {
+
+    public Card cardOne = new Card(Card.Suit.Coins, Card.FaceName.Ace, "txt");
+
+    public static void main(String[] args) {
+        test();
+    }
+
+    public static void test() {
+
+    }
+}
