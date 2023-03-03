bd7beeafac3c7c304ba4c4f7b055d9deb505657a
Abby Brown <abrown01@carroll.edu>
2023-03-03 11:19:31 -0700

    Fix enumeration and start unit test

--- src/CardTests.java
+++ src/CardTests.java
@@ -1,12 +1,22 @@
+
 public class CardTests {
 
-    public Card cardOne = new Card(Card.Suit.Coins, Card.FaceName.Ace, "txt");
+    public static Card cardOne = new Card(Card.Suit.Coins, Card.FaceName.Ace, "txt");
+    public static Card cardTwo = new Card(Card.Suit.Sticks, Card.FaceName.Three, "txt");
 
     public static void main(String[] args) {
-        test();
+        testHighestCards(cardOne, cardTwo);
     }
 
-    public static void test() {
+    public static void testHighestCards(Card card, Card other) {
+        System.out.println("Testing High Level Cards (getStrength)...");
+        if (card.getStrength() == 9 && other.getStrength() == 8) {
+            System.out.println("   pass");
+        } else {
+            System.err.println("   failed getStrength");
+        }
+
+
 
     }
 }
